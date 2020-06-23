ac510d3efc3d14a317a47d29c8d6c59687c83b33
Landerson <clandokca@gmail.com>
2020-06-23 11:26:45 -0300

    Início do versionamento.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux'
+import RepositoryList from './components/RepositoryList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import store from './store'
 
-export default App;
+const App = () => <Provider store={store}><RepositoryList /></Provider>
+
+export default App
